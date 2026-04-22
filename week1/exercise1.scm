c46642b65dedcb049734bb7b619763f275d1f700
Victor <voogunsanya@gmail.com>
2026-04-22 19:33:33 -0400

    Scuffed exercise 1 solutions

--- week1/exercise1.scm
+++ week1/exercise1.scm
@@ -1,16 +1,49 @@
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (good-enough? guess x)
+       (< (abs (- (square guess) x)) 0.001))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+(sqrt 9)
+(sqrt 16)
+
+(sqrt 25)
+
+(sqrt 81)
+
+(sqrt 144)
+
+(sqrt 225)
+
 (define (square x) (* x x))
-square
+
 (square 5)
-(square 21)
-(square (square 3))
 
-(define (sum-of-squares x y)
-  (+ (square x) (square y)))
-(sum-of-squares 8 9)
+(define (cube-root-improve guess x)
+  (/ 
+    (+
+      (/ x (square guess))
+      (+ guess guess))
+    3))
+(define (cube-good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+(define (cube x) (* x x x))
+(cube 3)
+(define (cube-root-iter guess x)
+  (if (cube-good-enough? guess x)
+    guess
+    (cube-root-iter (cube-root-improve guess x) x)))
+(define (cube-root x)
+  (cube-root-iter 1.0 x))
 
-(define (abs x)
-  (cond ((> x 0) x)
-	((= x 0) 0)
-	((< x 0) (- x))))
-(abs 19)
+(cube-root 27)
+
+(cube-root 64)
+
 
